9c86b82baf89ac2c6b76b026cce2f079193a8983
Phu Nguyen <phu.nguyen@nfq.asia>
2023-06-09 16:28:30 +0700

    Tree traversal

--- src/day1/BTInOrder.ts
+++ src/day1/BTInOrder.ts
@@ -1,3 +1,15 @@
-export default function in_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+  if (!curr) return path;
 
+  walk(curr.left, path);
+  
+  path.push(curr.value);
+  
+  walk(curr.right, path);
+
+  return path;
+} 
+
+export default function in_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
